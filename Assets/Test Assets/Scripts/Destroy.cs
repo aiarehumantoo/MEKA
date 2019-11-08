5b91754e7b00500dac166c0e0ae7678791b35687
aiarehumantoo <juuso.t.hamalainen@gmail.com>
2019-11-09 00:35:15 +0200

    Destroy VFX

---
+++ Assets/Test Assets/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Delete());
+    }
+
+    IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(this.gameObject);
+    }
+}
