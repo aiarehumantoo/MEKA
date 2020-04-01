8fd30dc1f711b3f9f96021e2b0cecbf05bc69b81
aiarehumantoo <juuso.t.hamalainen@gmail.com>
2020-04-01 23:05:33 +0300

    Updated projectiles

--- Assets/MEKA/Scripts/Weapons/Projectiles/ProjectileBase.cs
+++ Assets/MEKA/Scripts/Weapons/Projectiles/ProjectileBase.cs
@@ -7,19 +7,8 @@ using UnityEngine;
 /*
  * TODO;
  * 
- * Dealing damage;
- *      Save gameobject instead of colliders? For cases when enemy has multiple colliders
- *      So that damage is applied just once, regardless of how many colliders enemy has
- *      saving all colliders works too
  *  
- *  Replace lifetime with default maximum distance
- *         For projectiles that explode after short distance (ie. MIRV)
- *         Doubles as deleting projectiles that hit nothing
- *         
- *  Todo; filter hits->damage
- *          Direct hit damage should be dealt only to first target
- *              Currently looping all hits within raycast distance
- *              Set raycast to stop after first hit? -> no need to do checks
+ *              
  */
 
 
@@ -37,13 +26,12 @@ public class ProjectileBase : MonoBehaviour
 
     // Simulation
     private bool move = true; // Projectile simulation
-    protected int shootableMask; // A layer mask so the raycast only hits things on the shootable layer.
     private Vector3 movementVector;
-
-    List<Collider> ignoreColliders = new List<Collider>(); // Save damaged colliders so that same damage is not dealt twice
-
     protected float maximumRange = 100.0f; // Maximum range
     private float totalTraveledDistance = 0.0f;
+
+    // List of hit (root) gameobjects
+    List<GameObject> hitTargets = new List<GameObject>(); // Filters out multihits. (Several colliders or direct + splash)
 
     //**************************************************
     public void Setup(float damage, float splashdmg)
@@ -56,9 +44,6 @@ public class ProjectileBase : MonoBehaviour
     protected virtual void Start()
     {
         movementVector = transform.forward * projectileSpeed;
-
-        // Create a layer mask for the Shootable layer.
-        shootableMask = LayerMask.GetMask("Enemy", "Environment");
     }
 
     //**************************************************
@@ -72,16 +57,18 @@ public class ProjectileBase : MonoBehaviour
         RaycastHit closestHit = new RaycastHit();
         closestHit.distance = Mathf.Infinity;
         bool foundHit = false;
+        bool reachedMaxDistance = false;
 
         // Predict projectile movement
         Vector3 nextPosition = transform.position + movementVector * Time.deltaTime;
-        var predictedMovement = nextPosition - transform.position;
+        Vector3 predictedMovement = nextPosition - transform.position;
 
         // Sphere cast from current to next position
+        int shootableMask = LayerMask.GetMask("Enemy", "Environment"); // Layers projectile can collide with
         RaycastHit[] hits = Physics.SphereCastAll(transform.position, projectileRadius, predictedMovement.normalized, predictedMovement.magnitude, shootableMask);
         foreach (var hit in hits)
         {
-            if (IsHitValid(hit) && hit.distance < closestHit.distance)
+            if (hit.distance < closestHit.distance)
             {
 #if SHOWDEBUGLOG
                 Debug.Log("PROJECTILE HIT SOMETHING");
@@ -93,7 +80,7 @@ public class ProjectileBase : MonoBehaviour
         }
 
         // Check for maximum range
-        var traveledDistance = predictedMovement.magnitude;
+        float traveledDistance = predictedMovement.magnitude;
         if (totalTraveledDistance + traveledDistance >= maximumRange) // Reached max projectile range
         {
             Vector3 maxdistPos = transform.position + predictedMovement.normalized * (maximumRange - totalTraveledDistance);
@@ -105,24 +92,34 @@ public class ProjectileBase : MonoBehaviour
                 Debug.Log("PROJECTILE REACHED MAXIMUM RANGE");
 #endif
 
-                foundHit = true;
+                foundHit = false; // Set to false in case hit was found outside of maximum range
+                reachedMaxDistance = true;
                 closestHit.point = maxdistPos;
 
                 // Add remaining distance
                 totalTraveledDistance += Vector3.Distance(transform.position, maxdistPos);
-                Debug.Log(totalTraveledDistance);
             }
         }
 
         // Hit something or reached maximum range
-        if (foundHit)
+        if (foundHit || reachedMaxDistance)
         {
             // Projectile hit something, update position
             move = false;
             nextPosition = closestHit.point;
 
+            if (foundHit)
+            {
+                // Deal direct hit damage
+                DealDamage(closestHit.transform.root.gameObject, damagePerShot);
+            }
+
             // Create explosion
             Explosion(closestHit);
+
+#if SHOWDEBUGLOG
+            Debug.Log(totalTraveledDistance);
+#endif
         }
 
         // Move projectile
@@ -136,80 +133,45 @@ public class ProjectileBase : MonoBehaviour
     }
 
     //**************************************************
-    private bool IsHitValid(RaycastHit hit)
-    {
-        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
-        {
-#if SHOWDEBUGLOG
-            Debug.Log("PROJECTILE HIT ENVIRONMENT");
-#endif
-            return true;
-        }
-
-        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-        {
-            if (!ignoreColliders.Contains(hit.collider))
-            {
-#if SHOWDEBUGLOG
-                Debug.Log("PROJECTILE HIT ENEMY. " +damagePerShot +" damage");
-#endif
-
-                // Save collider(s) projectile hit directly
-                Collider[] hitCollider = hit.collider.gameObject.GetComponentsInChildren<Collider>();
-                ignoreColliders.AddRange(hitCollider);
-
-                return true;
-            }
-        }
-
-        //if hits enemy
-        // Deal direct damage, exclude target from taking splash damage
-
-        return false;
-    }
-
-    //**************************************************
     protected virtual void Explosion(RaycastHit hit)
     {
         // Hit location
-        var location = hit.point; // Remember; hit.point = point of collision; hit.transform.position = location of hit target
+        Vector3 location = hit.point; // Remember; hit.point = point of collision; hit.transform.position = location of hit target
 
         // Destroy projectile & create explosion SFX
         Destroy(this.gameObject);
         Instantiate(explosionSFX, location, transform.rotation);
 
         // Layers that can receive damage
-        var damageLayer = LayerMask.GetMask("Player", "Enemy");
+        int damageLayer = LayerMask.GetMask("Player", "Enemy");
 
-        // Check for enemies inside the splash radius
+        // Check for targets inside the splash radius
         Collider[] hitColliders = Physics.OverlapSphere(location, splashRadius, damageLayer);
-        for (int i = 0; i < hitColliders.Length; i++)
+        foreach (var collider in hitColliders)
         {
-            if (hitColliders[i].gameObject.layer == LayerMask.NameToLayer("Player"))
-            {
-#if SHOWDEBUGLOG
-                Debug.Log("SPLASH DID SELFDAMAGE");
-#endif
-            }
-            if (hitColliders[i].gameObject.layer == LayerMask.NameToLayer("Enemy"))
-            {
-                if (!ignoreColliders.Contains(hitColliders[i]))
-                {
-#if SHOWDEBUGLOG
-                    Debug.Log("SPLASH HIT ENEMY. " + splashDamage +" damage");
-#endif
-
-                    // Save collider(s) inside splash radius    // No need to save colliders, this is done just once. needed for !multihits?
-                    Collider[] hitCollider = hitColliders[i].gameObject.GetComponentsInChildren<Collider>();                             //does this get every collider or just childrens
-                    ignoreColliders.AddRange(hitCollider);
-
-                    // Save gameobject, in case of hitting multiple colliders of same enemy. Saving all targets colliders works too?
-                    // Physics.OverlapSphere gets all hit colliders
-                    // but after first time dealing dmg, enemys every collider is in the list and rest are skipped
-                    // gameobject check might be more efficient even if functionally identical
-                }
-            }
+            DealDamage(collider.transform.root.gameObject, splashDamage);
         }
+    }
+
+    //**************************************************
+    private void DealDamage(GameObject target, float damage)
+    {
+        // Hit nothing or environment
+        if (!target || target.layer == LayerMask.NameToLayer("Environment"))
+        {
+            return;
+        }
+
+        // Make sure target was not already hit
+        if (!hitTargets.Contains(target))
+        {
+            // Save target (root gameobject)
+            hitTargets.Add(target);
+
+            // Damage target
+            //hit.collider.gameObject.GetComponent<HealthBase>().ReceiveDamage();
+        }
+
     }
 
     //**************************************************
@@ -222,7 +184,7 @@ public class ProjectileBase : MonoBehaviour
         Gizmos.color = radiusColor;
         Gizmos.DrawSphere(transform.position, projectileRadius);
 
-        // Splash
+        // Splash radius
         Color radiusColorSplash = Color.red * 0.5f;
         Gizmos.color = radiusColorSplash;
         Gizmos.DrawSphere(transform.position, splashRadius);
